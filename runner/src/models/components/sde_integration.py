Fix integration off by one error (thanks tara) + change to OTCFM + Add time_conder from PIS

runner/src/models/components/sde_integration.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+
 from src.models.components.sdes import VEReverseSDE
 
 
@@ -12,18 +13,19 @@ def euler_maruyama_step(sde: VEReverseSDE, t: torch.Tensor, x: torch.Tensor, dt:
     x_next = x + drift + diffusion
     return x_next, drift
 
+
 def integrate_pfode(
     sde: VEReverseSDE,
     x0: torch.Tensor,
     num_integration_steps: int,
-    reverse_time: bool = True
+    reverse_time: bool = True,
 ):
     start_time = 1.0 if reverse_time else 0.0
     end_time = 1.0 - start_time
 
     times = torch.linspace(
         start_time, end_time, num_integration_steps + 1, device=x0.device
-    )
+    )[:-1]
 
     x = x0
     samples = []
@@ -34,19 +36,20 @@ def integrate_pfode(
 
     return torch.stack(samples)
 
+
 def integrate_sde(
     sde: VEReverseSDE,
     x0: torch.Tensor,
     num_integration_steps: int,
     reverse_time: bool = True,
-    no_grad=True
+    no_grad=True,
 ):
     start_time = 1.0 if reverse_time else 0.0
     end_time = 1.0 - start_time
 
     times = torch.linspace(
         start_time, end_time, num_integration_steps + 1, device=x0.device
-    )
+    )[:-1]
 
     x = x0
     samples = []