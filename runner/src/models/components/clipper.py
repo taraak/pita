34caed1aa91ecce5e36bafc0c016cf76aae1f917
tara akhound-sadegh <tara.ak.sa@gmail.com>
2024-12-04 20:50:01 -0500

    clipping if molecule now inside clipper

--- runner/src/models/components/clipper.py
+++ runner/src/models/components/clipper.py
@@ -1,5 +1,6 @@
 import torch
 from typing import Optional
+from src.energies.base_energy_function import BaseEnergyFunction
 
 _EPSILON = 1e-6
 
@@ -11,11 +12,17 @@ class Clipper:
         should_clip_log_rewards: bool,
         max_score_norm: Optional[float] = None,
         min_log_reward: Optional[float] = None,
+        energy_function: Optional[BaseEnergyFunction] = None,
     ):
         self._should_clip_scores = should_clip_scores
         self._should_clip_log_rewards = should_clip_log_rewards
         self.max_score_norm = max_score_norm
         self.min_log_reward = min_log_reward
+        self.energy_function = energy_function
+
+        self.is_molecule = False
+        if energy_function is not None:
+            self.is_molecule = energy_function.is_molecule
 
     @property
     def should_clip_scores(self) -> bool:
@@ -26,13 +33,26 @@ class Clipper:
         return self._should_clip_log_rewards
 
     def clip_scores(self, scores: torch.Tensor) -> torch.Tensor:
+        if self.is_molecule:
+            scores = scores.reshape(
+                -1,
+                self.energy_function.n_particles,
+                self.energy_function.n_spatial_dim,
+            )
+
         score_norms = torch.linalg.vector_norm(scores, dim=-1).detach()
 
         clip_coefficient = torch.clamp(
             self.max_score_norm / (score_norms + _EPSILON), max=1
         )
 
-        return scores * clip_coefficient.unsqueeze(-1)
+        clipped_scores = scores * clip_coefficient.unsqueeze(-1)
+
+        if self.is_molecule:
+            clipped_scores = clipped_scores.reshape(-1,
+                                                    self.energy_function.dimensionality
+            )
+        return clipped_scores
 
     def clip_log_rewards(self, log_rewards: torch.Tensor) -> torch.Tensor:
         return log_rewards.clamp(min=self.min_log_reward)
