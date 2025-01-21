fixed bug with negative sign

runner/src/models/components/sde_integration.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from src.models.components.sdes import VEReverseSDE
+from src.models.components.utils import sample_cat_sys
 from src.utils.data_utils import remove_mean
 from src.energies.base_energy_function import BaseEnergyFunction
 from contextlib import contextmanager
@@ -16,17 +17,6 @@ from contextlib import contextmanager
 #     # Update the state
 #     x_next = x + drift + diffusion
 #     return x_next, drift
-
-
-
-def sample_cat_sys(bs, logits):
-    u = torch.rand(size=(1,), dtype=torch.float64)
-    u = (u + 1/bs*torch.arange(bs)) % 1.0
-    clipped_weights = torch.clip(torch.softmax(logits, dim=-1), 1e-6, 1.0)
-    bins = torch.cumsum(clipped_weights, dim=-1)
-    ids = np.digitize(u, bins.cpu())
-    return ids, None
-
 
 @contextmanager
 def conditional_no_grad(condition):
@@ -78,16 +68,16 @@ def euler_maruyama_step(
         drift_At[i*batch_size:(i+1)*batch_size] = drift_At_i
 
     # drift_Xt, drift_At = sde.f(t, x, resampling_interval)
-    drift_Xt = drift_Xt * dt * inverse_temp
+    drift_Xt = drift_Xt * dt
     drift_At = drift_At * dt
 
     if t.dim() == 0:
         # repeat the same time for all points if we have a scalar time
         t = t * torch.ones_like(x).to(x.device)
-    diffusion = diffusion_scale * sde.g(t, x) * np.sqrt(dt) * torch.randn_like(x).to(x.device)
+    diffusion = sde.diffusion(t, x, diffusion_scale, inverse_temp)
 
     # Update the state
-    x_next = x + drift_Xt + diffusion
+    x_next = x + drift_Xt + diffusion * np.sqrt(dt)
     a_next = a + drift_At
 
     if resampling_interval==-1 or step+1 % resampling_interval != 0:

runner/src/models/components/sdes.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from src.models.components.utils import compute_laplacian
 
 class SDE(torch.nn.Module):
@@ -19,6 +20,72 @@ class SDE(torch.nn.Module):
     
     def g(self, t, x):
         return self.diffusion(t, x)
+
+
+class VEReverseSDE(torch.nn.Module):
+    noise_type = "diagonal"
+    sde_type = "ito"
+
+    def __init__(self, model, noise_schedule, scale_diffusion=False):
+        super().__init__()
+        self.model = model
+        self.noise_schedule = noise_schedule
+        self.scale_diffusion = scale_diffusion
+
+    def f(self, t, x, resampling_interval=None, inverse_temp=1.0):
+        if t.dim() == 0:
+            # repeat the same time for all points if we have a scalar time
+            t = t * torch.ones(x.shape[0]).to(x.device)
+
+        with torch.enable_grad():
+            x.requires_grad_(True)
+            t.requires_grad_(True)
+
+            nabla_Ut = self.model(t, x)
+
+            if self.scale_diffusion:
+                drift_X = 0.5 * (inverse_temp+1) * nabla_Ut * self.g(t, x).pow(2).unsqueeze(-1)
+            else:
+                drift_X = inverse_temp * nabla_Ut * self.g(t, x).pow(2).unsqueeze(-1)
+
+            drift_A = torch.zeros(x.shape[0]).to(x.device)
+            
+            if resampling_interval is None:
+                return  drift_X.detach(), drift_A.detach()
+            
+            drift_A = 0.5 * (inverse_temp-1) * inverse_temp * (self.g(t, x)[:, None] * nabla_Ut).pow(2).sum(-1) 
+                
+        return  drift_X.detach(), drift_A.detach()
+    
+
+    def diffusion(self, t, x, diffusion_scale, inverse_temp=1.0):
+        if t.dim() == 0:
+            # repeat the same time for all points if we have a scalar time
+            t = t * torch.ones_like(x).to(x.device)
+
+        if self.scale_diffusion:
+            return diffusion_scale * self.g(t, x) * torch.randn_like(x).to(x.device) / np.sqrt(inverse_temp)
+        
+        return diffusion_scale * self.g(t, x) * torch.randn_like(x).to(x.device)
+
+    def g(self, t, x):
+        g = self.noise_schedule.g(t)
+        return g
+    
+class RegVEReverseSDE(VEReverseSDE):
+    def f(self, t, x):
+        dx = super().f(t, x[..., :-1])
+        quad_reg = 0.5 * dx.pow(2).sum(dim=-1, keepdim=True)
+        return torch.cat([dx, quad_reg], dim=-1)
+
+    def g(self, t, x):
+        g = self.noise_schedule.g(t)
+        if g.ndim > 0:
+            return g.unsqueeze(1)
+        return torch.cat(
+            [torch.full_like(x[..., :-1], g), torch.zeros_like(x[..., -1:])], dim=-1
+        )
+    
 
 
 # class VEReverseSDE(torch.nn.Module):
@@ -65,52 +132,3 @@ class SDE(torch.nn.Module):
 #     def g(self, t, x):
 #         g = self.noise_schedule.g(t)
 #         return g
-
-
-class VEReverseSDE(torch.nn.Module):
-    noise_type = "diagonal"
-    sde_type = "ito"
-
-    def __init__(self, model, noise_schedule):
-        super().__init__()
-        self.model = model
-        self.noise_schedule = noise_schedule
-
-    def f(self, t, x, resampling_interval=None, inverse_temp=1.0):
-        if t.dim() == 0:
-            # repeat the same time for all points if we have a scalar time
-            t = t * torch.ones(x.shape[0]).to(x.device)
-
-        with torch.enable_grad():
-            x.requires_grad_(True)
-            t.requires_grad_(True)
-            epsilon_t = self.g(t, x).pow(2) / 2 
-
-            nabla_Ut = self.model(t, x)
-            drift_X = nabla_Ut * self.g(t, x).pow(2).unsqueeze(-1)
-
-            drift_A = torch.zeros(x.shape[0]).to(x.device)
-            
-            if resampling_interval is None:
-                return  drift_X.detach(), drift_A.detach()
-            
-            drift_A = epsilon_t * (-nabla_Ut.pow(2).sum(-1) * (inverse_temp-1) * inverse_temp)
-        return  drift_X.detach(), drift_A.detach()
-
-    def g(self, t, x):
-        g = self.noise_schedule.g(t)
-        return g
-    
-class RegVEReverseSDE(VEReverseSDE):
-    def f(self, t, x):
-        dx = super().f(t, x[..., :-1])
-        quad_reg = 0.5 * dx.pow(2).sum(dim=-1, keepdim=True)
-        return torch.cat([dx, quad_reg], dim=-1)
-
-    def g(self, t, x):
-        g = self.noise_schedule.g(t)
-        if g.ndim > 0:
-            return g.unsqueeze(1)
-        return torch.cat(
-            [torch.full_like(x[..., :-1], g), torch.zeros_like(x[..., -1:])], dim=-1
-        )