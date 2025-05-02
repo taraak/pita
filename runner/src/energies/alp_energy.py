fixed bug in forces normalization

runner/src/energies/alp_energy.py
@@ -7,7 +7,7 @@ import mdtraj as md
 import numpy as np
 import openmm
 import torch
-import torchvision
+from contextlib import contextmanager
 from bgflow import OpenMMBridge, OpenMMEnergy
 from lightning.pytorch.loggers import WandbLogger
 from matplotlib.colors import LogNorm
@@ -27,6 +27,15 @@ from src.models.components.optimal_transport import torus_wasserstein
 from src.utils.data_utils import remove_mean
 
 logger = logging.getLogger(__name__)
+
+
+@contextmanager
+def conditional_grad(condition):
+    if condition:
+        with torch.enable_grad():
+            yield
+    else:
+        yield
 
 
 class ALPEnergy(BaseMoleculeEnergy):
@@ -107,14 +116,21 @@ class ALPEnergy(BaseMoleculeEnergy):
             ),
         )
 
-    def __call__(self, samples: torch.Tensor, return_force=False) -> torch.Tensor:
+    def maybe_normalize(self, x: torch.Tensor) -> torch.Tensor:
         if self.should_normalize:
-            samples = self.unnormalize(samples)
-        logprob = -self.openmm_energy.energy(samples).squeeze(-1)
-        if return_force:
-            force = self.openmm_energy.force(samples)
-            return logprob, force
-        return logprob
+            return self.normalize(x)
+        return x
+
+    def __call__(self, samples: torch.Tensor, return_force=False) -> torch.Tensor:
+        with conditional_grad(return_force):
+            samples.requires_grad = True
+            logprob = -self.openmm_energy.energy(self.maybe_normalize(samples)).squeeze(-1)
+            if return_force:
+                force = torch.autograd.grad(logprob.sum(), samples, create_graph=False)[0]
+                samples.requires_grad = False
+                return logprob, force
+            samples.requires_grad = False
+            return logprob
 
     def setup_test_set(self):
         data = np.load(self.data_path_test, allow_pickle=True)

runner/src/models/energytemp_module.py
@@ -309,7 +309,7 @@ class energyTempModule(BaseLightningModule):
             xt=xt,
             energy_function=energy_function,
             predicted_x0=predicted_x0_scorenet,
-            true_nabla_U0=x0_forces,
+            true_force=x0_forces,
             weights=None, #TODO: should we use lambda_t here?
         )
         dem_energy_loss = self.get_dem_energy_loss(
@@ -342,9 +342,6 @@ class energyTempModule(BaseLightningModule):
         time_mask = ht >= h_threshold
         if not time_mask.any():
             return torch.zeros_like(predicted_x0_scorenet)
-        # xt = xt[time_mask]
-        # ht = ht[time_mask]
-        # predicted_x0_scorenet = predicted_x0_scorenet[time_mask]
 
         score_loss = torch.sum((predicted_x0_scorenet - xt) ** 2, dim=(-1))
         score_loss[~time_mask] = 0.0
@@ -382,7 +379,7 @@ class energyTempModule(BaseLightningModule):
         xt: torch.Tensor,
         energy_function: BaseEnergyFunction,
         predicted_x0: torch.Tensor,
-        true_nabla_U0: Optional[torch.Tensor] = None,
+        true_force: Optional[torch.Tensor] = None,
         weights: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         if self.hparams.loss_weights["target_score"] == 0:
@@ -397,13 +394,13 @@ class energyTempModule(BaseLightningModule):
         xt = xt[time_mask]
         predicted_x0 = predicted_x0[time_mask]
 
-        if true_nabla_U0 is None:
+        if true_force is None:
             energy = -energy_function(x0).sum()
-            score = torch.autograd.grad(energy, x0, create_graph=True)[0]
+            nabla_U0 = torch.autograd.grad(energy, x0, create_graph=True)[0] # -score
         else:
-            score = true_nabla_U0[time_mask]
-        score = self.hparams.clipper.clip_scores(score)
-        x0 = xt - score * ht[:, None]
+            nabla_U0 = - true_force[time_mask]
+        nabla_U0 = self.hparams.clipper.clip_scores(nabla_U0)
+        x0 = xt - nabla_U0 * ht[:, None]
         target_score_loss = torch.sum((x0 - predicted_x0) ** 2, dim=(-1))
 
         if weights is not None:
@@ -635,7 +632,6 @@ class energyTempModule(BaseLightningModule):
             elif prefix == "val":
                 true_x0_samples = energy_function.sample_val_set(num_samples)
             true_x0_energies, true_x0_forces = energy_function(true_x0_samples, return_force=True)
-
             loss = self.model_step(
                 true_x0_samples,
                 true_x0_energies,
@@ -1087,7 +1083,6 @@ class energyTempModule(BaseLightningModule):
             init_energies, init_forces = self.energy_functions[temp_index](
                 init_states, return_force=True
             )
-
             if temp_index == 0:
                 self.buffers[temp_index].add(init_states, init_energies, init_forces)
 