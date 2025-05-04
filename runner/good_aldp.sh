0f8b14c39b7667d5f35e4990d4266d7635f07ac9
tara akhound-sadegh <tara.ak.sa@gmail.com>
2025-05-04 16:45:22 -0400

    al3 script

---
+++ runner/good_aldp.sh
@@ -0,0 +1,25 @@
+HYDRA_FULL_ERROR=1 python src/train.py +trainer.num_sanity_val_steps=0 \
+model=energytemp \
+experiment=alp_energytemp_good \
+trainer=ddp model.resampling_interval=1 \
+tags=["test","ALDP","wise_silence"] \
+trainer.check_val_every_n_epoch=100 \
+model.debias_inference=True \
+model.resample_at_end=True \
+model.loss_weights.energy_matching=1.0 \
+model.loss_weights.energy_score=1.0 \
+model.loss_weights.score=1.0 \
+model.loss_weights.target_score=0.01 \
+model.loss_time_threshold.score=0 \
+model.inference_batch_size=512 \
+model.training_batch_size=1024 \
+model.num_negative_time_steps=0 \
+model.post_mcmc_steps=5 \
+model.end_resampling_step=850 \
+model/net=dit \
+model.num_epochs_per_temp=[200,200,200,200]
+#model.net.hidden_size=768 \
+#debug=short \
+#+model.only_train_score=True \
+#+energy.debug_train_on_test=True \
+#debug=short \
