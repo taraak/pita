5c60470c4cdd18ce730f341e6693063142c05fff
Alex Tong <alexandertongdev@gmail.com>
2025-05-04 11:28:33 -0400

    Fix resumable (does not work with multirun)

--- runner/scripts/alex/v3_dit.sh
+++ runner/scripts/alex/v3_dit.sh
@@ -1,12 +1,12 @@
 RUN_NAME="bigdit"
-HYDRA_FULL_ERROR=1 python src/train.py -m +trainer.num_sanity_val_steps=0 \
+HYDRA_FULL_ERROR=1 python src/train.py +trainer.num_sanity_val_steps=0 \
 model=energytemp \
 experiment=alp_energytemp \
 trainer=ddp model.resampling_interval=1 \
 tags=["test","ALDP","big"] \
 model.noise_schedule.sigma_min=0.005 \
-trainer.check_val_every_n_epoch=10 \
-trainer.max_epochs=50 \
+trainer.check_val_every_n_epoch=2 \
+trainer.max_epochs=6 \
 model.dem.num_training_epochs=0 \
 model.debias_inference=False \
 model.loss_weights.energy_matching=0.0 \
